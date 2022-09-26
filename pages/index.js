04fd7119d0b4f17005b29bc4b7ac0e335fb473cc
Laudin <gastonlaudin@gmail.com>
2022-09-26 13:53:42 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,93 @@
+import Head from 'next/head'
+import { social, stack, experience } from '../public/data/data'
+
+export default function Home() {
+
+  const experiences = experience.map(item => (
+    <li key={item.name} className='bg-background rounded-md p-1 inline'>
+      <p className='inline'>{item.name}</p>
+    </li>
+  ))
+  const stacks = stack.map(item => (
+    <li key={item.name} className='flex justify-center'>
+      <span></span>
+      <img 
+        src={item.logo}
+        width="64"
+        height="48"
+      />
+    </li>
+  ))
+  const socials = social.map(item => (
+    <li key={item.name}>
+      <a target='_blanck' href={item.link} >
+        <span className='sr-only'>{item.name}</span>
+        <item.icon className='h-8 w-8 hover:bg-zinc-800 rounded-md p-1' aria-hidden='true' />
+      </a>
+    </li>
+  ))
+
+  return (
+    <>
+      <Head>
+        <title>Gastón Ladin</title>
+      </Head>
+
+      <div className='bg-background min-h-screen'>
+        <main className=' text-white text-sm flex min-h-screen flex-col justify-center gap-8 m-auto py-10 max-w-5xl'>
+            <header className=' bg-main w-10/12 mx-auto p-3 rounded-md flex items-center overflow-hidden'>
+              <img 
+                src='/images/profile.png'
+                className=' rounded-full mx-8 w-20 h-auto object-cover'  
+              />
+              <h3 className='text-xl'>Gastón Laudin</h3>
+            </header>
+            
+            <div className='bg-main w-10/12 mx-auto rounded-md overflow-hidden
+                [&>*]:p-5 
+                [&>*]:flex 
+                [&>*]:flex-col 
+                [&>*]:md:flex-row 
+              [&>*]:border-b-background 
+                [&>*]:border-b-2 
+                [&>*]:border-b-solid 
+                [&>*]:md:p-7'>
+              <div className=''>
+                <h3 className='w-4/12 text-gray-400 mb-4 md:mb-0 font-bold'>E-mail</h3>
+                <p>gastonlaudin@gmail.com</p>
+              </div>
+
+              <div>
+                <h3 className='w-4/12 text-gray-400 mb-4 md:mb-0 font-bold'>Profession</h3>
+                <p>Full-Stack Web Developer</p>
+              </div>
+
+              <div>
+                <h3 className='w-4/12 text-gray-400 mb-4 md:mb-0 font-bold'>Experience</h3>
+                <ul>
+                  {experiences}
+                </ul>
+              </div>
+
+              <div>
+                <h3 className='w-4/12 text-gray-400 mb-4 md:mb-0 font-bold'>Preffered Stack</h3>
+                <ul className='flex gap-x-16 flex-wrap w-8/12'>
+                  {stacks}
+                </ul>
+              </div>
+
+              <div>
+                <h3 className='w-4/12 text-gray-400 mb-4 md:mb-0 font-bold'>Social</h3>
+                <ul className='flex gap-8 flex-wrap'>
+                  {socials}
+                </ul>
+              </div>
+
+            </div>
+
+        </main>
+      </div>
+
+    </>
+  )
+}
---
+++ public/data/data.js
@@ -0,0 +1,75 @@
+import { 
+   iconGithub,
+   iconLinkedIn,
+   iconTwitter,
+   iconFacebook,
+   iconInstagram, } from "../icons/icons"
+
+export const stack = [
+   {
+      name: 'Javascript',
+      logo: '/images/stack/javascript.svg',
+   },
+   {
+      name: 'Node',
+      logo: '/images/stack/nodejs.svg',
+   },
+   {
+      name: 'Express',
+      logo: '/images/stack/expressjs.svg',
+   },
+   {
+      name: 'React',
+      logo: '/images/stack/react.svg',
+   },
+   {
+      name: 'Next',
+      logo: '/images/stack/nextjs.svg',
+   },
+   {
+      name: 'Angular',
+      logo: '/images/stack/angular.svg',
+   },
+   {
+      name: 'MongoDB',
+      logo: '/images/stack/mongodb.svg',
+   },
+   {
+      name: 'Postgresql',
+      logo: '/images/stack/postgresql.svg',
+   },
+]
+
+export const experience = [
+   {
+      name: 'Web Developer',
+   },
+]
+
+export const social = [
+   {
+      name: 'GitHub',
+      link: 'https://github.com/Laudin',
+      icon: iconGithub
+   },
+   {
+      name: 'LinkedIn',
+      link: 'https://www.linkedin.com/in/gaston-laudin/',
+      icon: iconLinkedIn
+   },
+   {
+      name: 'Instagram',
+      link: 'https://www.instagram.com/ton.chicken/',
+      icon: iconInstagram
+   },
+   {
+      name: 'Twitter',
+      link: 'https://twitter.com/TonLaudin',
+      icon: iconTwitter
+   },
+   {
+      name: 'Facebook',
+      link: 'https://www.facebook.com/gaston.luadin/',
+      icon: iconFacebook
+   },
+]
