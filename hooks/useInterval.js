579b71082867e5ee7ef21d81fe245f4a038b70e9
Gastón Laudin <gaston.laudin@loesen.co>
2023-10-03 15:49:53 -0300

    Update useInterval.js

--- hooks/useInterval.js
+++ hooks/useInterval.js
@@ -1,29 +1,41 @@
-import { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { version } from '../config/version';
 
 export default function useInterval() {
+  const buildIdRef = React.useRef(null);
   const interval = 3000;
-  const [reload, setReload] = useState(false);
-
-  const savedCallback = async () => {
-    const response = await fetch('/api/build-id');
-    const { buildId } = await response.json();
-
-    console.log(buildId);
-
-    if (buildId && version.version && buildId !== version.version) {
-      setReload(true);
-    } else {
-    }
-  };
+  const [shouldReload, setShouldReload] = React.useState(false);
 
   useEffect(() => {
-    if (interval !== null) {
-      const id = setInterval(savedCallback, interval);
+    // I found there is a  __NEXT_DATA__ IDed script tag
+    // It contains JSON that includes buildId
+    buildIdRef.current = JSON.parse(
+      document.querySelector('#__NEXT_DATA__').textContent
+    ).buildId;
 
-      return () => clearInterval(id);
-    }
+    // check if the buildId path exists
+    const id = setInterval(() => {
+      const request = new XMLHttpRequest();
+      request.open(
+        'HEAD',
+        `/_next/static/${buildIdRef.current}/_buildManifest.js`,
+        false
+      );
+      request.setRequestHeader('Pragma', 'no-cache');
+      request.setRequestHeader('Cache-Control', 'no-cache');
+      request.setRequestHeader(
+        'If-Modified-Since',
+        'Thu, 01 Jun 1970 00:00:00 GMT'
+      );
+      request.send(null);
+      console.log(request.status);
+      setShouldReload(request.status === 404);
+    }, interval);
+
+    return () => {
+      clearInterval(id);
+    };
   }, []);
 
-  return [reload];
+  return [shouldReload];
 }
